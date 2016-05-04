372b9122108d3a9ce621f7150ad64f8b9f585436
Eduards Bezverhijs <edzis@inbox.lv>
2016-05-04 20:35:11 +0300

    Improve translations handling

--- locale/update_pot.sh
+++ locale/update_pot.sh
@@ -13,10 +13,10 @@ then
 	exit
 fi
 
-xgettext -o timekpr.pot ../src/gui/about_dialog.ui
-xgettext --join-existing -o timekpr.pot ../src/gui/client.ui
-xgettext --join-existing -o timekpr.pot ../src/gui/main_window.ui 
-xgettext --join-existing -o timekpr.pot ../src/timekpr-gui.py
-xgettext --join-existing -o timekpr.pot ../src/timekpr-client.py
+xgettext --add-comments=TRANSLATORS -o timekpr.pot ../src/gui/about_dialog.ui
+xgettext --add-comments=TRANSLATORS --join-existing -o timekpr.pot ../src/gui/client.ui
+xgettext --add-comments=TRANSLATORS --join-existing -o timekpr.pot ../src/gui/main_window.ui 
+xgettext --add-comments=TRANSLATORS --join-existing -o timekpr.pot ../src/timekpr-gui.py
+xgettext --add-comments=TRANSLATORS --join-existing -o timekpr.pot ../src/timekpr-client.py
 
 msgmerge -U $UPD_LANG/LC_MESSAGES/timekpr.po ./timekpr.pot
